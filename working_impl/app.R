ea1ba03bf52dff5ecbd244bacf7bbebdc40bbf8a
Kyle Husmann <kyle.husmann@gmail.com>
2025-06-07 13:51:05 -0700

    add purrr

--- working_impl/app.R
+++ working_impl/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(purrr)
 
 app_styles <- HTML("
 .centered-content {
@@ -33,10 +34,9 @@ server <- function(input, output, session) {
   dynamic_observers <- reactiveVal(list())
 
   dataset <- shiny::reactive({
-    Filter(
-      is.numeric,
-      get(input$dataset_select, envir = asNamespace("datasets"))
-    )
+    input$dataset_select |>
+      get(envir = asNamespace("datasets")) |>
+      keep(is.numeric)
   })
 
   output$column_select_ui <- renderUI({
@@ -49,20 +49,17 @@ server <- function(input, output, session) {
   })
 
   output$cards_ui <- renderUI({
-    lapply(isolate(dynamic_observers()), \(i) i$destroy())
+    walk(isolate(dynamic_observers()), \(i) i$destroy())
 
     dynamic_observers(
-      lapply(input$column_select, function(i) {
+      map(input$column_select, function(i) {
         close_btn_id <- paste0(i, "_close")
         observeEvent(
           input[[close_btn_id]],
           {
             updateSelectInput(
               inputId = "column_select",
-              selected = Filter(
-                \(j) j != i,
-                input$column_select
-              )
+              selected = discard(input$column_select, \(j) j == i)
             )
           },
           ignoreInit = TRUE,
@@ -70,7 +67,7 @@ server <- function(input, output, session) {
       })
     )
 
-    lapply(input$column_select, function(i) {
+    map(input$column_select, function(i) {
       fluidRow(
         class = "card",
         column(
