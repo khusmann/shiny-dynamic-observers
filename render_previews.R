0c001033c032defa9b5e275cc05796e531e66d42
Kyle Husmann <kyle.husmann@gmail.com>
2025-06-08 21:54:41 -0700

    try adding render_previews

---
+++ render_previews.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(webshot2)
+library(processx)
+library(httr)
+
+# Path to your app (can be ".")
+app_dir <- "src/solution"
+port <- 1234
+url <- sprintf("http://127.0.0.1:%d", port)
+
+# Start the app in background
+shiny_proc <- process$new(
+  "Rscript",
+  c("-e", sprintf("shiny::runApp('%s', port=%d, launch.browser=FALSE)", app_dir, port)),
+  stdout = "|", stderr = "|"
+)
+
+# Wait for the app to start (check up to 20 times)
+for (i in 1:20) {
+  Sys.sleep(1)
+  result <- tryCatch(GET(url), error = function(e) NULL)
+  if (!is.null(result) && status_code(result) == 200) {
+    break
+  }
+  if (i == 20) stop("App did not start in time")
+}
+
+# Take screenshot
+webshot(url, file = "build/preview.png", vwidth = 576, vheight = 450)
+
+# Kill the app
+shiny_proc$kill()
